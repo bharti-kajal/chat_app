78681254f368bdda684f3084dc98f422ebc68117
bharti-kajal <kajalbharti990@gmail.com>
2025-03-21 08:42:06 +0530

    user schema

---
+++ server/models/user.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    fullname: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profilePic: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+const userModel = mongoose.model('user', userSchema);
+export default userModel;
